7d39f415f6607e3f8ca168d389b1c917086b0eaf
Bowens <hwbrzzl@qq.com>
2022-09-07 23:11:04 +0800

    Optimize config.ts

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,11 +3,22 @@ import { defaultTheme } from "vuepress";
 import taskLists from 'markdown-it-task-lists'
 import { getZhSidebar } from "./config/sidebar/zh";
 import { getEnSidebar } from "./config/sidebar/en";
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 
 export default defineUserConfig({
   lang: "en-US",
   title: "Goravel",
   description: "A Golang web application framework",
+  head: [
+    [
+      "script",
+      {
+        "data-ad-client": "ca-pub-4978322804450032",
+        async: true,
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      }
+    ]
+  ],
   locales: {
     "/": {
       lang: "en-US",
@@ -20,6 +31,11 @@ export default defineUserConfig({
       description: "Golang WEB 应用框架",
     },
   },
+  plugins: [
+    googleAnalyticsPlugin({
+      id: "G-HJQNEG5H69"
+    }),
+  ],
   extendsMarkdown: md => {
 	  md.use(taskLists)
 	},
