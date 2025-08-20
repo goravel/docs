86c1d964742def85a5cffb8adf1662c187323816
耗子 <haozi@loli.email>
2025-08-20 18:41:23 +0800

    feat: add zh_TW

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vitepress'
 import { shared } from './shared'
 import { config as en } from './en'
 import { config as zh_CN } from './zh_CN'
+import { config as zh_TW } from './zh_TW'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   ...shared,
   locales: {
     root: { label: 'English', ...en },
-    zh_CN: { label: '简体中文', ...zh_CN }
+    zh_CN: { label: '简体中文', ...zh_CN },
+    zh_TW: { label: '繁體中文', ...zh_TW },
   }
 })
